1175e1d063ec7d6ce757c6c84cbe0ef6b9562847
alisoneoz <alisonestephanyoz@gmail.com>
2025-07-31 09:58:52 -0400

    install and test tailwindcss

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import reactLogo from './assets/images/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <div>
+      <div className='bg-red-100'>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
