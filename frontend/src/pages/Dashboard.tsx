4abd6819e4abf37780ee40cb98a12a181cbb6b75
alisoneoz <alisonestephanyoz@gmail.com>
2025-08-04 08:30:24 -0400

    fix importation

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -8,7 +8,7 @@ import {
     TableRow,
   } from "../components/ui/table";
   import { CircleUserRound } from "lucide-react";
-  import React, { useEffect, useState } from "react";
+  import { useEffect, useState } from "react";
 
 type User = {
     id: number;
