db7cc9725886f0cbff0e2959d75a6c345815c603
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-08-05 16:59:53 -0300

    feat: adds product to children in router, router file

--- frontend/src/routing/router.tsx
+++ frontend/src/routing/router.tsx
@@ -6,26 +6,24 @@ import NotFoundPage from "../pages/NotFoundPage";
 import Layout from "../components/Layout";
 import Dashboard from "../pages/Dashboard";
 import { PrivateRoute } from "../components/features/PrivateRoute";
+import Products from "../pages/products/Products";
 
 const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Layout />,
-      children: [
-        { index: true, element: <Home /> },
-        { path: "login", element: <Login /> },
-        { path: "register", element: <Register /> },
-        { 
-          element: <PrivateRoute />,
-          children: [
-            { path: "dashboard", element: <Dashboard />}
-          ]
-        },
-        { path: "*", element: <NotFoundPage /> }
-      ]
-    }
-  ]);
-  
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "login", element: <Login /> },
+      { path: "register", element: <Register /> },
+      { path: "products", element: <Products /> },
+      {
+        element: <PrivateRoute />,
+        children: [{ path: "dashboard", element: <Dashboard /> }],
+      },
+      { path: "*", element: <NotFoundPage /> },
+    ],
+  },
+]);
 
-
-export default router
+export default router;
