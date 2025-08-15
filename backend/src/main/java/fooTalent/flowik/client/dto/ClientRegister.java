908d452bddde65a22c00af8e6783f29f0b0ee843
Jeronimo Herrera <jeronimoh1990@gmail.com>
2025-08-14 21:13:20 -0300

    fix errors

--- backend/src/main/java/fooTalent/flowik/client/dto/ClientRegister.java
+++ backend/src/main/java/fooTalent/flowik/client/dto/ClientRegister.java
@@ -1,11 +1,11 @@
 package fooTalent.flowik.client.dto;
 
-import fooTalent.misino.validation.OnlyLettersAndSpaces;
+import fooTalent.flowik.validation.OnlyLettersAndSpaces;
 import jakarta.validation.constraints.*;
-import org.springframework.format.annotation.DateTimeFormat;
+
 
 import java.time.LocalDate;
-import java.util.Date;
+
 
 public record ClientRegister(
         @NotBlank(message = "El nombre del Client es obligatorio.")
@@ -24,12 +24,10 @@ public record ClientRegister(
 
         @NotBlank(message = "La dirección del Cliente es obligatoria.")
         @Size(min = 10, max = 100, message = "La Direccion del Cliente debe tener entre 10 y 100 caracteres")
-        @OnlyLettersAndSpaces
         String direction_provider,
 
         @Pattern(regexp = "^[a-zA-Z0-9_!#$%&’*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$",message = "Correo de Cliente invalido")
         @NotBlank(message = "La dirección del Correo Electronico del Cliente es obligatoria.")
-        @Size(min = 6, max = 100, message = "La Direccion del Correo Electronicodel Cliente debe tener entre 6 y 100 caracteres")
         String emil_client,
 
         //@NotBlank(message = "La Fecha de ingreso del Cliente obligatoria.")
