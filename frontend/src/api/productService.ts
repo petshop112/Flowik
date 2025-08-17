11de04a6497c14527a291c9ac8a1d555f99253b4
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-08-17 16:38:11 -0300

    refactor: adds getUserIdFromStorage function to getAllProducts function in productService file to search id from user in storage

--- frontend/src/api/productService.ts
+++ frontend/src/api/productService.ts
@@ -1,14 +1,24 @@
 import axios, { AxiosError } from "axios";
+import { getUserIdFromStorage } from "../utils/storage";
 
 export const API_BASE_URL = import.meta.env.VITE_API_URL;
 
 const getAllProducts = async (token: string) => {
+  const id = getUserIdFromStorage();
+
+  if (!id) {
+    throw new Error("User ID not found in storage");
+  }
+
   try {
-    const response = await axios.get(`${API_BASE_URL}products/getProducts/20`, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
+    const response = await axios.get(
+      `${API_BASE_URL}products/getProducts/${id}`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
     return response.data;
   } catch (error) {
     const axiosError = error as AxiosError;
