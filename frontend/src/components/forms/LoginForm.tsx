1860eb6f5276132988b20368c2fc66ee7328ba42
Alen Oviedo <afoviedola@gmail.com>
2025-08-20 12:23:51 -0500

    Fix: successful register

--- frontend/src/components/forms/LoginForm.tsx
+++ frontend/src/components/forms/LoginForm.tsx
@@ -4,6 +4,7 @@ import { Formik, Form, Field, ErrorMessage } from 'formik';
 import { useAppSelector, useAppDispatch } from '../../hooks/reduxHooks';
 import { loginUser, selectAuth } from '../../features/auth/authSlice';
 import type { FormikHelpers } from 'formik';
+import { ExclamationCircleIcon } from '@heroicons/react/24/outline';
 import EyeIconSlash from '../../icons/eye-slash.svg?react';
 import EyeIcon from '../../icons/eye.svg?react';
 import * as Yup from 'yup';
@@ -42,85 +43,116 @@ const LoginForm = () => {
       validationSchema={validationSchema}
       onSubmit={handleSubmit}
     >
-      <Form className="space-y-8">
-        <div>
-          <label
-            htmlFor="email"
-            className="font-albert block text-[16px] leading-[19.2px] font-semibold text-[#042D95]"
-          >
-            Email
-          </label>
-          <Field
-            name="email"
-            type="email"
-            id="email"
-            placeholder="example@gmail.com"
-            data-test="email"
-            className="mt-1 block w-full rounded-[6px] border border-[#CBD5E1] bg-white px-4 py-2 shadow-[0_2px_2px_0_rgba(0,0,0,0.04)] outline-none focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200"
-          />
-          <ErrorMessage name="email" component="div" className="mt-1 text-sm text-red-500" />
-        </div>
-
-        <div>
-          <div className="flex items-center justify-between">
+      {({ errors, touched }) => (
+        <Form className="space-y-8">
+          <div>
             <label
-              htmlFor="password"
+              htmlFor="email"
               className="font-albert block text-[16px] leading-[19.2px] font-semibold text-[#042D95]"
             >
-              Contraseña
+              Email
             </label>
-            <a
-              href="/recover-password"
-              className="text-[#5685FA] font-['Albert Sans'] text-[16px] font-normal leading-[120%]"
-            >
-              ¿Olvidaste tu contraseña?
-            </a>
-          </div>
-          <div className="relative">
             <Field
-              name="password"
-              type={showPassword ? 'text' : 'password'}
-              id="password"
-              data-test="password"
-              placeholder="*********"
-              className="mt-1 mb-2 block w-full rounded-[6px] border border-[#CBD5E1] bg-white px-4 py-2 shadow-[0_2px_2px_0_rgba(0,0,0,0.04)] outline-none focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200"
+              name="email"
+              type="email"
+              id="email"
+              placeholder="example@gmail.com"
+              data-test="email"
+              className={`mt-1 block w-full rounded-[6px] border px-4 py-2 shadow-[0_2px_2px_0_rgba(0,0,0,0.04)] outline-none ${
+                errors.email && touched.email
+                  ? 'border-red-500 focus:border-red-500 focus:ring-2 focus:ring-red-200'
+                  : 'border-[#CBD5E1] bg-white focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200'
+              }`}
             />
-            <button
-              type="button"
-              onClick={() => setShowPassword(!showPassword)}
-              className="absolute inset-y-0 right-3 flex items-center bg-transparent p-0 text-gray-400 hover:text-gray-600 focus:outline-none"
-            >
-              {showPassword ? (
-                <EyeIconSlash className="h-5 w-5 stroke-current text-gray-400 hover:text-gray-600" />
-              ) : (
-                <EyeIcon className="h-5 w-5 stroke-current text-gray-400 hover:text-gray-600" />
+            <ErrorMessage
+              name="email"
+              render={(msg) => (
+                <div className="mt-1 flex items-center gap-1 text-sm text-red-500">
+                  <ExclamationCircleIcon className="h-4.5 w-4.5" strokeWidth={2.5} />
+                  <span>{msg}</span>
+                </div>
               )}
-            </button>
+            />
           </div>
-          <ErrorMessage name="password" component="div" className="mt-1 text-sm text-red-500" />
-        </div>
 
-        {error && <div className="text-center text-sm text-red-500">{error}</div>}
+          <div>
+            <div className="flex items-center justify-between">
+              <label
+                htmlFor="password"
+                className="font-albert block text-[16px] leading-[19.2px] font-semibold text-[#042D95]"
+              >
+                Contraseña
+              </label>
+              <a
+                href="/recover-password"
+                className="font-['Albert Sans'] text-[16px] leading-[120%] font-normal text-[#5685FA]"
+              >
+                ¿Olvidaste tu contraseña?
+              </a>
+            </div>
+            <div className="relative">
+              <Field
+                name="password"
+                type={showPassword ? 'text' : 'password'}
+                id="password"
+                data-test="password"
+                placeholder="*********"
+                className={`mt-1 block w-full rounded-[6px] border px-4 py-2 shadow-[0_2px_2px_0_rgba(0,0,0,0.04)] outline-none ${
+                  errors.password && touched.password
+                    ? 'border-red-500 focus:border-red-500 focus:ring-2 focus:ring-red-200'
+                    : 'border-[#CBD5E1] bg-white focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200'
+                }`}
+              />
+              <button
+                type="button"
+                onClick={() => setShowPassword(!showPassword)}
+                className="absolute inset-y-0 right-3 flex items-center bg-transparent p-0 text-gray-400 hover:text-gray-600 focus:outline-none"
+              >
+                {showPassword ? (
+                  <EyeIconSlash className="h-5 w-5 stroke-current text-gray-400 hover:text-gray-600" />
+                ) : (
+                  <EyeIcon className="h-5 w-5 stroke-current text-gray-400 hover:text-gray-600" />
+                )}
+              </button>
+            </div>
+            <ErrorMessage
+              name="password"
+              render={(msg) => (
+                <div className="mt-1 flex items-center gap-1 text-sm text-red-500">
+                  <ExclamationCircleIcon className="h-4.5 w-4.5" strokeWidth={2.5} />
+                  <span>{msg}</span>
+                </div>
+              )}
+            />
+          </div>
 
-        <button
-          type="submit"
-          className="font-['Albert Sans'] mt-12 flex h-12 w-full items-center justify-center gap-2 rounded-[6px] bg-[#5685FA] px-6 py-3 text-[16px] leading-[120%] font-normal text-white transition duration-300 hover:bg-[#4170e8] disabled:opacity-50"
-          disabled={loading}
-          data-test="login-button"
-        >
-          {loading ? 'Cargando...' : 'Iniciar sesión'}
-        </button>
+          {error && (
+            <div className="mt-2 flex items-center justify-center gap-1 text-sm text-red-500">
+              <ExclamationCircleIcon className="h-4.5 w-4.5" strokeWidth={2.5} />
+              {error}
+            </div>
+          )}
 
-        <p className="font-['Albert Sans'] text-center text-[16px] leading-[120%] font-normal text-[#5685FA]">
-          ¿Primera vez aquí?{' '}
-          <a
-            href="/register"
-            className="font-['Albert Sans'] ml-1 border-b border-[#5685FA] text-[16px] leading-[120%] font-semibold text-[#5685FA] hover:border-b-2"
+          <button
+            type="submit"
+            className="font-['Albert Sans'] mt-12 flex h-12 w-full items-center justify-center gap-2 rounded-[6px] bg-[#5685FA] px-6 py-3 text-[16px] leading-[120%] font-normal text-white transition duration-300 hover:bg-[#4170e8] disabled:opacity-50"
+            disabled={loading}
+            data-test="login-button"
           >
-            Regístrate
-          </a>
-        </p>
-      </Form>
+            {loading ? 'Cargando...' : 'Iniciar sesión'}
+          </button>
+
+          <p className="font-['Albert Sans'] text-center text-[16px] leading-[120%] font-normal text-[#5685FA]">
+            ¿Primera vez aquí?{' '}
+            <a
+              href="/register"
+              className="font-['Albert Sans'] ml-1 border-b border-[#5685FA] text-[16px] leading-[120%] font-semibold text-[#5685FA] hover:border-b-2"
+            >
+              Regístrate
+            </a>
+          </p>
+        </Form>
+      )}
     </Formik>
   );
 };
