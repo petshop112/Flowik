e0358bede8a7b755f71cd334ed4444fa7b2d64c7
RodolfoBischoff <rodolfo27.rb@gmail.com>
2025-08-27 12:46:10 -0300

    notification

--- backend/src/main/java/fooTalent/flowik/notifications/controller/NotificationController.java
+++ backend/src/main/java/fooTalent/flowik/notifications/controller/NotificationController.java
@@ -1,4 +1,42 @@
 package fooTalent.flowik.notifications.controller;
+import fooTalent.flowik.config.SecurityUtil;
+import fooTalent.flowik.notifications.dto.NotificationDto;
+import fooTalent.flowik.notifications.enums.NotificationType;
+import fooTalent.flowik.notifications.mappers.NotificationMapper;
+import fooTalent.flowik.notifications.services.NotificationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/api/notifications")
 public class NotificationController {
+
+        @Autowired
+        private NotificationService service;
+
+        @GetMapping
+        public List<NotificationDto.NotificationDTO> getAllNotifications() {
+            String user = SecurityUtil.getAuthenticatedEmail();
+            return service.listAll(user).stream()
+                    .map(NotificationMapper::toDTO)
+                    .collect(Collectors.toList());
+        }
+
+        @GetMapping("/type/{type}")
+        public List<NotificationDto.NotificationDTO> getNotificationsByType(@PathVariable NotificationType type) {
+            String user = SecurityUtil.getAuthenticatedEmail();
+            return service.listByType(user, type).stream()
+                    .map(NotificationMapper::toDTO)
+                    .collect(Collectors.toList());
+        }
+
+        @PostMapping("/read/{id}")
+        public void markNotificationAsRead(@PathVariable Long id) {
+            String user = SecurityUtil.getAuthenticatedEmail();
+            service.markAsRead(id, user);
+        }
+
 }
